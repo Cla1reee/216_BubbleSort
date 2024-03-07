0f5f37c4f8b3b72f715b550b52255dd56f0794db
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-03-07 17:11:47 +0700

    Add project files.

---
+++ AlgoritmaSehati/AlgoritmaSehati.cpp
@@ -0,0 +1,15 @@
+// AlgoritmaSehati.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
