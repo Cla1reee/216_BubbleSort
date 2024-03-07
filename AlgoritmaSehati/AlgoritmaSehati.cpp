4b9a35cac8230fe1e6ed607b6203707923b58444
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-03-07 17:28:27 +0700

    membuat variabel

--- AlgoritmaSehati/AlgoritmaSehati.cpp
+++ AlgoritmaSehati/AlgoritmaSehati.cpp
@@ -1,15 +1,11 @@
-// AlgoritmaSehati.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 using namespace std;
 
 int arr[20];
 int n;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+     
 }
 
 
