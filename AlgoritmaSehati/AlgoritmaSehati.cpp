ee9b4741e5a9deda79159093c1bb470e94c1b693
Hapis <152777554+Cla1reee@users.noreply.github.com>
2024-03-20 22:23:50 +0700

    Update AlgoritmaSehati.cpp

--- AlgoritmaSehati/AlgoritmaSehati.cpp
+++ AlgoritmaSehati/AlgoritmaSehati.cpp
@@ -1,69 +1,56 @@
 #include <iostream>
+
 using namespace std;
 
 int arr[20];
 int n;
-
-void input() {
-    while (true) {
-        cout << "masukan banyaknya elemen pada array : ";
+void input(){
+    while (true)
+    {
+        cout <<"masukan banyaknya elemen array : ";
         cin >> n;
         if (n <= 20)
             break;
         else {
-            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
-
+            cout << "\nArray dapat mempunyai maksimal 20 eleman.\n";
         }
     }
     cout << endl;
-    cout << "===============" << endl;
-    cout << "masukan Elemen Array" << endl;
-    cout << "===============" << endl;
-
-    for (int i = 0; i < n; i++) {
-        cout << "data ke-" << (i + 1) << ": ";
+    cout << "====================" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "====================" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "data ke-" << (i + 1 ) << ": ";
         cin >> arr[i];
-
     }
-
-}
-void bubbleSortArray() { //procedur untuk mengurutkan array dengan metode bublle sort
-
-    int pass = 1; // step 1
-
+}void bubbleSortArray(){ //procedur untuk mengurutkan array
+    int pass= 1; //step 1
     do {
-        for (int j = 0; j <= n - 1 - pass; j++) { // step 2
-            if (arr[j] > arr[j + 1]) { //step 3
+        for(int j = 0; j <= n - 1 - pass; j++){ //step 2
+            if(arr[j] > arr[j + 1]){ //step 3
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
-
-
             }
         }
-        pass = pass + 1; //step 4
-
-    } while (pass <= n); //step 5
-}
-void display() {
+        pass = pass + 1; //step 4 
+    } while (pass <= n - 1); //step 5
+} void display (){
     cout << endl;
-    cout << "===================================" << endl;
+    cout << "=================================" << endl;
     cout << "Element Array yang telah tersusun" << endl;
-    cout << "===================================" << endl;
-    for (int j = 0; j < n; j++) {
-        cout << arr[j] << endl; // Output setiap element array pada garis baru
-
+    cout << "=================================" << endl;
+    for( int j = 0; j < n; j++){
+        cout << arr[j] << endl; //output setiap element array pada garis baru
     }
-    cout << "jumlah pass = " << n - 1 << endl; //menampilkan total angka dari elemen yang benar
-    cout << endl;
+    cout << "jumlah pass = " << n-1  << endl;
+    cout << endl;  
 }
-int main()
-{
-    input();  //memanggil prosedur untuk menginput data
-    bubbleSortArray();  //memanggil prosedur bubblesort untuk proses mengurutkan data
-    display();//memanggil prosedur untuk menampilkan data yang telah urut
-    system("pause");
 
-
-    return 0;
+int main(){
+ input();
+        bubbleSortArray();
+        display();
+        system("pause");
 }
