b7131185f46d95ba0ebf2420c8286218e90f38e8
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2026-03-10 15:01:51 +0700

    Logika Bubble Sort

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -37,6 +37,20 @@ void display() {
     cout << endl;
 }
 
+void bubbleSortArray() {
+    int pass = 1;
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass = pass + 1;
+    } while (pass <= n - 1);
+}
+
 int main() {
     return 0;
 }
