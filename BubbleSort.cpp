cb8b749b1e0db84a97626d1f6f46c41d5227c5b3
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2026-03-10 15:01:18 +0700

    Fungsi Output (Display)

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -25,6 +25,18 @@ void input() {
     }
 }
 
+void display() {
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl;
+    }
+    cout << "jumlah pass = " << n - 1 << endl;
+    cout << endl;
+}
+
 int main() {
     return 0;
 }
